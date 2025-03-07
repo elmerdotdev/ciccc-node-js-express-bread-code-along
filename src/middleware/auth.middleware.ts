461c63e911ddbd394d3e85217858b2a138892d77
Elmer Balbin <contact@elmer.dev>
2025-03-06 16:09:07 -0800

    update auth.middleware.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -3,6 +3,14 @@ import { Request, Response, NextFunction } from 'express'
 // Blacklisted IPs
 const blacklistIP = ['129.212.43.11', '352.62.72.1', '636.27.14.1']
 
+/**
+ * Middleware to block requests from blacklisted IPs.
+ *
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ * @param {NextFunction} next - Express next function.
+ * @returns {void} Blocks request if IP is blacklisted, otherwise continues.
+ */
 export function auth (req: Request, res: Response, next: NextFunction) {
   console.log(`${req.ip} is visiting the server`)
   if (blacklistIP.includes(req.ip ?? '')) {
