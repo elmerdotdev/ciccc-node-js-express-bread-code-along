e088fabd69d0c7b48d846341bc0642640e87e0d6
Elmer Balbin <contact@elmer.dev>
2025-03-06 16:07:35 -0800

    update user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,17 +4,28 @@ import { v4 as uuidv4 } from 'uuid'
 
 const userRouter = Router()
 
-const users: User[] = [
-  { id: 'hkjo32ir3ur903', firstname: "John", lastname: "Smith" },
-  { id: 'joiflfpofiefwf', firstname: "Jane", lastname: "Doe" }
-]
+const users: User[] = []
 
-// Get users   http://localhost:3500/users - Browse
+/**
+ * Get users
+ * 
+ * @route GET /users
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ * @returns {void} Responds with users list object.
+ */
 userRouter.get('/', (req: Request, res: Response) => {
   res.status(200).json(users)
 })
 
-// Get user by id    http://localhost:3500/users/1 - Read
+/**
+ * Get user by ID
+ * 
+ * @route GET /users/:id
+ * @param {Request} req - Express request containing user ID.
+ * @param {Response} res - Express response object.
+ * @returns {void} Responds with user matching ID.
+ */
 userRouter.get('/:id', (req: Request<{ id: string }>, res: Response) => {
   const { id } = req.params
   const user = users.find(user => user.id === id)
@@ -25,7 +36,14 @@ userRouter.get('/:id', (req: Request<{ id: string }>, res: Response) => {
   res.status(200).json(user)
 })
 
-// Add user      http://localhost:3500/users - Add
+/**
+ * Adds a new user to the system.
+ *
+ * @route POST /users
+ * @param {Request<{}, {}, Omit<User, 'id'>>} req - Express request containing the user data in the body.
+ * @param {Response} res - Express response object.
+ * @returns {void} Responds with the newly created user.
+ */
 userRouter.post("/", (req: Request<{}, {}, Omit<User, 'id'>>, res: Response) => {
   const newUser: User = {
     id: uuidv4(),
@@ -36,7 +54,14 @@ userRouter.post("/", (req: Request<{}, {}, Omit<User, 'id'>>, res: Response) => 
   res.status(201).json(newUser)
 })
 
-// Update user by id      http://localhost:3500/users/1 - Edit
+/**
+ * Updates a user by ID.
+ *
+ * @route PUT /users/:id
+ * @param {Request<{ id: string }, {}, Partial<User>>} req - Express request containing the user ID in params and the update fields in the body.
+ * @param {Response} res - Express response object.
+ * @returns {void} Responds with the updated user or an error if not found.
+ */
 userRouter.put("/:id", (req: Request<{ id: string }, {}, Partial<User>>, res: Response) => {
   const { id } = req.params
   const foundIndex = users.findIndex(user => user.id === id)
@@ -53,7 +78,15 @@ userRouter.put("/:id", (req: Request<{ id: string }, {}, Partial<User>>, res: Re
   res.status(200).json(updatedUser)
 })
 
-// Delete user by id    http://localhost:3500/users/1 - Delete
+
+/**
+ * Deletes a user by ID.
+ * 
+ * @route DELETE /users/:id
+ * @param {Request<{ id: string }>} req - Express request containing user id.
+ * @param {Response} res - Express response object.
+ * @returns {void} Responds with a message that user was deleted.
+ */
 userRouter.delete('/:id', (req: Request<{ id: string }>, res: Response) => {
   const { id } = req.params
   const foundIndex = users.findIndex(user => user.id === id)
@@ -65,7 +98,14 @@ userRouter.delete('/:id', (req: Request<{ id: string }>, res: Response) => {
   res.status(200).send("User was deleted!")
 })
 
-// Search user by firstname     http://localhost:3500/users/search/john
+/**
+ * Search users by firstname.
+ * 
+ * @route GET /users/search/:firstname
+ * @param {Request<{ firstname: string }>} req - Express request containing firstname.
+ * @param {Response} res - Express response object.
+ * @returns {void} Responds with user object.
+ */
 userRouter.get('/search/:firstname', (req: Request<{ firstname: string }>, res: Response) => {
   const { firstname } = req.params
   const foundUsers = users.filter(user => user.firstname.toLowerCase().includes(firstname.toLowerCase()))
